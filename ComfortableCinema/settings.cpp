cab0196be83589d359a42bcf53dad16b1feb8c24
Eryk07 <errryk333@gmail.com>
2020-05-03 14:19:45 +0200

    add default values to settings values and its SpinBoxes

--- ComfortableCinema/settings.cpp
+++ ComfortableCinema/settings.cpp
@@ -5,8 +5,21 @@ Settings::Settings(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Settings)
 {
+    rooms = 2;
+    fansCount = 4;
+    simulationTime = 300.0;
+    simulationStep = 4;
+
+    weather.CO2 = 415.5;
+    weather.temperature = 20.5;
+    weather.humidity = 0.6;
+
+    initConditions.CO2 = 415.5;
+    initConditions.temperature = 18.0;
+    initConditions.humidity = 0.5;
 
     ui->setupUi(this);
+    initSpinBoxes();
 }
 
 Settings::~Settings()
@@ -14,7 +27,55 @@ Settings::~Settings()
     delete ui;
 }
 
+void Settings::initSpinBoxes()
+{
+    // general
+    ui->spinRooms->setValue(rooms);
+    ui->spinRooms->setRange(1, 8);
 
+    ui->spinFansCount->setValue(fansCount);
+    ui->spinFansCount->setRange(1, 4);
+
+
+    ui->doubleSpinSimTime->setRange(1, 1000);
+    ui->doubleSpinSimTime->setSingleStep(0.5);
+    ui->doubleSpinSimTime->setDecimals(1);
+    ui->doubleSpinSimTime->setValue(simulationTime);
+
+    ui->simulationStepSpinBox->setValue(simulationStep);
+    ui->simulationStepSpinBox->setRange(1, 8);
+
+
+    //weather
+    ui->weatherCO2doubleSpinBox->setRange(350.0, 500.0);
+    ui->weatherCO2doubleSpinBox->setSingleStep(0.1);
+    ui->weatherCO2doubleSpinBox->setDecimals(1);
+    ui->weatherCO2doubleSpinBox->setValue(weather.CO2);
+
+    ui->weatherTempdoubleSpinBox->setSingleStep(0.1);
+    ui->weatherTempdoubleSpinBox->setDecimals(1);
+    ui->weatherTempdoubleSpinBox->setValue(weather.temperature);
+
+    ui->weatherHumiditydoubleSpinBox->setRange(0, 1);
+    ui->weatherHumiditydoubleSpinBox->setSingleStep(0.01);
+    ui->weatherHumiditydoubleSpinBox->setValue(weather.humidity);
+
+    //init inside conditions
+
+    ui->initCO2doubleSpinBox->setRange(350.0, 500.0);
+    ui->initCO2doubleSpinBox->setSingleStep(0.1);
+    ui->initCO2doubleSpinBox->setDecimals(1);
+    ui->initCO2doubleSpinBox->setValue(initConditions.CO2);
+
+
+    ui->initTempdoubleSpinBox->setSingleStep(0.1);
+    ui->initTempdoubleSpinBox->setDecimals(1);
+    ui->initTempdoubleSpinBox->setValue(initConditions.temperature);
+
+    ui->initHumiditydoubleSpinBox->setRange(0, 1);
+    ui->initHumiditydoubleSpinBox->setSingleStep(0.01);
+    ui->initHumiditydoubleSpinBox->setValue(initConditions.humidity);
+}
 
 void Settings::on_spinRooms_valueChanged(int _rooms)
 {
@@ -34,4 +95,34 @@ void Settings::on_doubleSpinSimTime_valueChanged(double _simTime)
 void Settings::on_simulationStepSpinBox_valueChanged(int _simStep)
 {
     simulationStep = _simStep;
+}
+
+void Settings::on_weatherCO2doubleSpinBox_valueChanged(double _weatherCO2)
+{
+    weather.CO2 = _weatherCO2;
+}
+
+void Settings::on_weatherTempdoubleSpinBox_valueChanged(double _weatherTemp)
+{
+    weather.temperature = _weatherTemp;
+}
+
+void Settings::on_weatherHumiditydoubleSpinBox_valueChanged(double _weatherHumidity)
+{
+    weather.humidity = _weatherHumidity;
+}
+
+void Settings::on_initCO2doubleSpinBox_valueChanged(double _initCO2)
+{
+    initConditions.CO2 = _initCO2;
+}
+
+void Settings::on_initTempdoubleSpinBox_valueChanged(double _initTemp)
+{
+    initConditions.temperature = _initTemp;
+}
+
+void Settings::on_initHumiditydoubleSpinBox_valueChanged(double _initHumidity)
+{
+    initConditions.humidity = _initHumidity;
 }
--- ComfortableCinema/settings.h
+++ ComfortableCinema/settings.h
@@ -2,6 +2,7 @@
 #define SETTINGS_H
 
 #include <QDialog>
+#include "factors/conditions.h"
 
 namespace Ui {
 class Settings;
@@ -16,9 +17,12 @@ public:
     ~Settings();
 
     int rooms;
-    int fansCount=3;
+    Conditions weather;
+    int fansCount;
+    Conditions initConditions;
     double simulationTime;
     int simulationStep;
+
 
 private slots:
     void on_spinFansCount_valueChanged(int _fansCount);
@@ -29,8 +33,21 @@ private slots:
 
     void on_simulationStepSpinBox_valueChanged(int _simStep);
 
+    void on_weatherCO2doubleSpinBox_valueChanged(double _weatherCO2);
+
+    void on_weatherTempdoubleSpinBox_valueChanged(double _weatherTemp);
+
+    void on_weatherHumiditydoubleSpinBox_valueChanged(double _weatherHumidity);
+
+    void on_initCO2doubleSpinBox_valueChanged(double _initCO2);
+
+    void on_initTempdoubleSpinBox_valueChanged(double _initTemp);
+
+    void on_initHumiditydoubleSpinBox_valueChanged(double _initHumidity);
+
 private:
     Ui::Settings *ui;
+    void initSpinBoxes();
 };
 
 #endif // SETTINGS_H
