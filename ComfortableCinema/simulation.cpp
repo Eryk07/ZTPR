331f45abc5c0d1ccb6bbaccb03baaeb500be7e77
Eryk07 <errryk333@gmail.com>
2020-06-23 14:36:05 +0200

    create Model class to integrate, connect factors changers to simulation

--- ComfortableCinema/simulation.cpp
+++ ComfortableCinema/simulation.cpp
@@ -7,31 +7,26 @@ Simulation::Simulation()
 
 void Simulation::setFactors()
 {
-    int peopleCount = 10; // domyślnie połączenie z PeopleManager ze ScreeningRoom
+    int peopleCount = 100; // domyślnie połączenie z PeopleManager ze ScreeningRoom
     int fansCount = 4; // domyślnie połączenie z Settings
+    double initSpeed = 0.5; // domyślnie połączenie z Settings
 
     this->factors.clear();
-    this->factors.push_back(new AirConditioning(fansCount));
+    this->factors.push_back(new AirConditioning(fansCount, initSpeed));
     this->factors.push_back(new People(peopleCount));
-}
-
-void Simulation::model( const state_type &x , state_type &dxdt , const double /* t */ )
-{
-        //tu dodać połączenie z wektorem Factor i poszczególnymi changerami
-        dxdt[0] = 2*x[1];
-        dxdt[1] = x[2]-x[0];
-        dxdt[2] = x[2];
 }
 
 void Simulation::simulate(std::vector<Conditions> &roomConditions, QVector<double> &roomTimesteps)
 {
     state_type initX(3);
-    initX[0] = 10000.0;     // inity z settingsów
-    initX[1] = 100;
-    initX[2] = 100;
+    initX[0] = 20.0;     // inity z settingsów
+    initX[1] = 410;
+    initX[2] = 0.65;
     std::vector<state_type>    states;
     std::vector<double>         times;
     Conditions momentCondition;
+    double roomVolume = 100000.0; // domyślnie z Settings
+    Model model(this->factors, roomVolume);
 
     size_t steps = odeint::integrate(model,
             initX , 0.0 , 10.0 , 0.1, observer(states, times));     // czas, kroki z settingsów
--- ComfortableCinema/simulation.h
+++ ComfortableCinema/simulation.h
@@ -31,6 +31,33 @@ private:
 
 };
 
+class Model
+{
+public:
+    Model(std::vector<Factor*> _factors, double _roomVolume)
+    {
+        factors = _factors;
+        roomValue = _roomVolume;
+    }
+    void operator()( const state_type &x , state_type &dxdt , const double /* t */ )
+    {
+            double sumTemp=0, sumCO2=0, sumHumidity=0;
+            for (int i = 0; i < this->factors.size(); i++)
+            {
+                sumTemp += this->factors[i]->changeTemperature(x[0], this->roomValue);
+                sumCO2 += this->factors[i]->changeCO2();
+                sumHumidity += this->factors[i]->changeHumidity(x[2], this->roomValue);
+            }
+
+            dxdt[0] = sumTemp;     // temperature
+            dxdt[1] = sumCO2;     // CO2
+            dxdt[2] = sumHumidity;     // humidity
+    }
+private:
+    std::vector<Factor*> factors;
+    double roomValue;
+};
+
 struct observer
 {
     std::vector<state_type>& states;
